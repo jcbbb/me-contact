131c6658eefa08d2e2c807136ec8cfff01a444ac
jc <avaz28082000@gmail.com>
2020-06-25 19:11:21 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// SVG icons
+const githubSvg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg> `;
+const twitterSvg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
+`;
+const telegramSvg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-send"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>
+`;
+const emailSvg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+`;
+
+const links = [
+    {
+        url: 'https://github.com/jcbbb',
+        svg: githubSvg,
+    },
+    {
+        url: 'https://twitter.com/Avazbek68487569',
+        svg: twitterSvg,
+    },
+    {
+        url: 'https://t.me/jcbbb',
+        svg: telegramSvg,
+    },
+    {
+        url: 'mailto:avaz28082000@gmail.com',
+        svg: emailSvg,
+    },
+];
+
+const meContact = document.createElement('ul');
+meContact.className = 'me-contact';
+
+links.forEach((link) => {
+    const contactItem = document.createElement('li');
+    contactItem.className = 'me-contact-item';
+    contactItem.setAttribute('tabindex', '0');
+    const contactLink = document.createElement('a');
+    contactLink.className = 'me-contact-link';
+    contactLink.setAttribute('href', link.url);
+    contactLink.setAttribute('tabindex', '-1');
+
+    contactLink.innerHTML = link.svg;
+    contactItem.append(contactLink);
+    meContact.append(contactItem);
+});
+
+document.body.append(meContact);
